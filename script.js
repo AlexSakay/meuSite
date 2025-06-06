7c5fbc8562ec190e437bb57618a34df0895b95b5
AlexSakay <alexsandro.email@gmail.com>
2025-06-06 19:42:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let botao = document.querySelector("#botao");
+
+botao.addEventListener("click",e =>{
+
+document.querySelector('#mensagem').innerHTML="Bem vindo a minha página!!!"
+
+}); 
+
+
+
+
+
